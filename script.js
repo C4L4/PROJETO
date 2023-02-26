d347065842d24db4b404f69c7100590cfef974c9
C4L4 <124742219+C4L4@users.noreply.github.com>
2023-02-26 00:23:54 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let img1 = document.getElementById("img1")
-let img2 = document.getElementById("img2")
-let img3 = document.getElementById("img3")
-let showimg1 = document.getElementById("showimg1")
-let body = document.getElementById("body")
-
-showimg1.addEventListener("click", ()=>{
-    img1.style.display = "none"
-})
